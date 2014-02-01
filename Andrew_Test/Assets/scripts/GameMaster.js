949426fbd1d7880df885c322d8f0d98a523d656c
colincove <cove.colin@gmail.com>
2014-02-01 01:34:44 -0800

    adding Unity test projects

---
+++ Andrew_Test/Assets/scripts/GameMaster.js
@@ -0,0 +1,73 @@
+﻿#pragma strict
+
+var zombie: GameObject;
+var tree : GameObject;
+
+var timer: float = 60; // set duration time in seconds in the Inspector
+var spawnDifference: float = 2;
+private var spawnTimer:float;
+
+var mapSprite : Sprite;
+static var  mapX : float;
+static var  mapY : float;
+
+static var  numLanes:int = 4;
+function Start () {
+
+	mapX = mapSprite.bounds.size.x;
+	mapY = mapSprite.bounds.size.y;
+   
+	Debug.Log("menu difficulty: "+MenuBehaviour.difficulty);
+	spawnTimer = timer;
+	
+	//placeTree();
+	
+
+}
+
+function Update () {
+	//4 lanes
+	spawnZombieTimer();
+}
+
+function spawnZombieTimer(){
+ 	timer -= Time.deltaTime;
+ 	
+ 	//spawn zombie every spawnDifference seconds
+ 	if (spawnTimer - timer >= spawnDifference){
+		spawnTimer = timer;
+		
+		//pick random lane to spawn
+		var lane = Random.Range(1, numLanes+1);
+		
+		//translate lane as Y coordinate
+		var placeSpawnY = lane*mapY/(numLanes+1)-mapY/2;
+		
+		//spawn at left and add velocity of 1
+		var spawnedZombie = Instantiate(zombie, new Vector3(-mapX/2-2,placeSpawnY,0), Quaternion.identity );
+		spawnedZombie.rigidbody2D.velocity.x=1.6;
+		spawnedZombie.GetComponent(ZombyBehaviour).setLane(lane);
+		
+		
+		//spawnedZombie.transform.localScale = Vector3(0.1,0.1,0.1);
+		
+ 	}
+  if (timer > 0){
+    //Debug.Log(timer);
+  } else {
+    Debug.Log("end timer");
+    
+  }
+}
+
+function placeTree(){
+
+	//pick random lane to spawn
+	var lane = Random.Range(1, numLanes+1);
+		
+	//translate lane as Y coordinate
+	var placeSpawnY = lane*mapY/(numLanes+1)-mapY/2;
+		
+	var spawnedTree = Instantiate(tree, new Vector3(0,placeSpawnY,0), Quaternion.identity );
+	spawnedTree.GetComponent(TreeBehaviour).setLane(lane);
+}
---
+++ Andrew_Test/Assets/scripts/ZombyBehaviour.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+
+
+var anim:Animator;
+private var onLane:int;
+private var newLane:int;
+private var changingLane:int;
+function Start () {
+
+}
+
+function Update () {
+	anim.SetFloat ("Speed", Mathf.Abs (rigidbody2D.velocity.x));
+	
+	if (changingLane!=0){
+		var mapY = GameMaster.mapY;
+		var newY = newLane*mapY/(GameMaster.numLanes+1)-mapY/2;
+		
+		if ((changingLane> 0 && transform.position.y>newY) || (changingLane< 0 && transform.position.y<newY)){
+			transform.position.y=newY;
+			
+			rigidbody2D.velocity.y=0;
+			rigidbody2D.velocity.x=1.6;
+			changingLane=0;
+			
+		}	
+	}
+}
+
+function  setLane(lane:int){
+	onLane = lane;
+}
+
+function  getLane():int{
+	return onLane;
+}
+
+function changeLane(){
+
+	
+	var cl = Random.Range(0,2);	 //+1 means go up, -1 means go down
+	if (cl==0) cl=-1;
+	
+	onLane+=cl;
+	
+	//prevent going past the map vertically
+	if (onLane==0) {
+		onLane+=2;
+		cl=1;
+	}
+	else if (onLane>GameMaster.numLanes){
+		onLane-=2;
+		cl=-1;
+	}
+	
+	newLane = onLane;
+	
+	rigidbody2D.velocity.y=1*cl;
+	rigidbody2D.velocity.x=0.5*-cl;
+	
+	changingLane = cl;
+}
