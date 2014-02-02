5c446c653b3741107d6cf3cb8c153abb133b11fe
Andrew Wong <andrew.wong.dgnt@gmail.com>
2014-02-02 05:19:08 -0800

    zombies walk through barricade after taking its hp to 0. Trailer has different states of breaking.

--- Andrew_Test/Assets/Game Objects/Barricade/BarricadeBehaviour.js
+++ Andrew_Test/Assets/Game Objects/Barricade/BarricadeBehaviour.js
@@ -1,18 +1,46 @@
 ﻿#pragma strict
 
-function Start () {
+var STARTING_HP:int;
+private var hp:int;
 
+var anim:Animator;
+
+function Start () {
+	hp = STARTING_HP*3;
+	
 }
 
 function Update () {
 
 }
 
+
+function OnTriggerEnter2D(collInfo : Collider2D){
+	if (collInfo.tag=="Zomby"){
+		if (hp<=0){
+			collInfo.GetComponent(ZombyBehaviour).anim.SetBool("Run",true);
+			collInfo.gameObject.GetComponent(ZombyBehaviour).anim.SetBool("Attack",false);
+			collInfo.rigidbody2D.velocity.x = 1.6;
+		}
+	}
+}
+
+
 function OnCollisionEnter2D (collInfo : Collision2D) {
 	if (collInfo.gameObject.tag=="Zomby"){
 		var zombieLane:int = collInfo.gameObject.GetComponent(GameObjectBehaviour).lane;
 		collInfo.gameObject.rigidbody2D.velocity.x=0;
 		collInfo.gameObject.rigidbody2D.velocity.y=0;
-		collInfo.gameObject.GetComponent(ZombyBehaviour).anim.SetTrigger("Attack");
+		collInfo.gameObject.GetComponent(ZombyBehaviour).anim.SetBool("Attack",true);
+		hp--;
+
+		if (hp<=0){
+			anim.SetTrigger("Break");
+			collider2D.enabled = false;	
+		}
+		else if (hp > 0 && hp%(STARTING_HP)==0){
+			anim.SetTrigger("Break");
+			
+		}
 	}
 }
