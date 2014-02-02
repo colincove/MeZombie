e48f81f8f53f7aad89fdf7dda502bf38e9863e62
Andrew Wong <andrew.wong.dgnt@gmail.com>
2014-02-01 17:49:36 -0800

    zombies spawn and bunch up behind a trailer

---
+++ Andrew_Test/Assets/Game Objects/Zombies/Zomby/ZombyBehaviour.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+
+var anim:Animator;
+var prevPositionY:float;
+function Start () {
+	prevPositionY = transform.position.y;
+}
+
+function Update () {
+	anim.SetFloat ("Speed", Mathf.Abs (rigidbody2D.velocity.x));
+
+
+
+}
+
