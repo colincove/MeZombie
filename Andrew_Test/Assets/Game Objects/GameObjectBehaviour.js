f7d3a6b030d7ba008fd0fd0e970466abe38b9b95
andrewwongdgnt <andrew.wong.dgnt@gmail.com>
2014-02-10 16:09:44 -0800

    modified human behaviour to attack first zombie only until it leaves its range. added blood splatter animation (just a red circle)

--- Andrew_Test/Assets/Game Objects/GameObjectBehaviour.js
+++ Andrew_Test/Assets/Game Objects/GameObjectBehaviour.js
@@ -1,10 +1,15 @@
 ﻿#pragma strict
 
 public var lane:int;
+public var yOffset:float;
 function Start () {
 
 }
 
 function Update () {
-	transform.localPosition.z  = transform.localPosition.y+5;
+	transform.localPosition.z  = transform.localPosition.y+5+yOffset;
+}
+
+function DestroyMe(){
+	Destroy(gameObject);
 }
--- Andrew_Test/Assets/Game Objects/Humans/HumanBehaviour.js
+++ Andrew_Test/Assets/Game Objects/Humans/HumanBehaviour.js
@@ -2,6 +2,8 @@
 
 var anim:Animator;
 var STARTING_HP:int;
+
+var bloodSplatter:GameObject;
 private var hp:int;
 
 function Start () {
@@ -12,28 +14,50 @@ function Update () {
 
 }
 
-function OnTriggerEnter2D(collInfo : Collider2D){
+private var firstZombie:Collider2D;
+
+//for some reason this fires multiple times before exiting
+//The code below assumes this, it may have to change if this behaviour changes
+function OnTriggerEnter2D(collInfo : Collider2D){ 
 	if (collInfo.tag=="Zomby"){
 		//attack zombie
 		
-		var zombieLane = collInfo.GetComponent(GameObjectBehaviour).lane;
-		var thisLane = GetComponent(GameObjectBehaviour).lane;
-		if (zombieLane == thisLane){ // only attack zombie in same lane
-			anim.SetBool("Attack",true);
-			anim.SetBool("Idle",false);
-			
-			
+		//attack only first zombie encountered until it leaves the hit area
+		if (firstZombie==null){			
+			firstZombie = collInfo;
+		}
+		if (firstZombie==collInfo){
+			var zombieLane = collInfo.GetComponent(GameObjectBehaviour).lane;
+			var thisLane = GetComponent(GameObjectBehaviour).lane;
+			if (zombieLane == thisLane){ // only attack zombie in same lane
+				anim.SetBool("Attack",true);
+				anim.SetBool("Idle",false);
 				
-			if (hp<=0){
-			//allow zombie to run past
-				collInfo.gameObject.GetComponent(ZombieBehaviour).anim.SetBool("Attack",false);
-				collInfo.gameObject.GetComponent(ZombieBehaviour).anim.SetBool("Run",true);
-			} else {
-			//slow zombie down while attacking
-				var force:float = 	collInfo.gameObject.GetComponent(ZombieBehaviour).speed*(-1/1000f);
-				collInfo.gameObject.rigidbody2D.AddForce(new Vector2(force,0));
-			}
+				
+					
+				if (hp<=0){
+				//allow zombie to run past
+					collInfo.gameObject.GetComponent(ZombieBehaviour).anim.SetBool("Attack",false);
+					collInfo.gameObject.GetComponent(ZombieBehaviour).anim.SetBool("Run",true);
+					firstZombie=null;
+				} else {
+				
+					//var force:float =     collInfo.gameObject.GetComponent(ZombieBehaviour).speed*(-3/1000f);
+               		//collInfo.gameObject.rigidbody2D.AddForce(new Vector2(force,0));
+               		
+               		//spawn blood
+               		
+               		var randomAllow = Random.Range(0, 100);
+               		if (randomAllow<30){
+               			var randomX = Random.Range(-0.2, 0.2);
+               			var randomY = Random.Range(-0.3, 0);
+               		
+               			Instantiate(bloodSplatter, new Vector3(collInfo.transform.position.x+randomX,collInfo.transform.position.y+randomY,collInfo.transform.position.z), Quaternion.identity );
+					}
 
+				}
+
+			}
 		}
 		
 		
@@ -42,12 +66,14 @@ function OnTriggerEnter2D(collInfo : Collider2D){
 
 function OnTriggerExit2D(collInfo : Collider2D){
 	if (collInfo.tag=="Zomby"){
-		
+		//stop attacking zombie
 		var zombieLane = collInfo.GetComponent(GameObjectBehaviour).lane;
 		var thisLane = GetComponent(GameObjectBehaviour).lane;
 		if (zombieLane == thisLane){ 
 			anim.SetBool("Attack",false);
 			anim.SetBool("Idle",true);
+			firstZombie=null;
+
 		}
 		
 	}
@@ -56,7 +82,7 @@ function OnTriggerExit2D(collInfo : Collider2D){
 function OnCollisionEnter2D (collInfo : Collision2D) {
 	if (collInfo.gameObject.tag=="Zomby"){
 		var zombieLane:int = collInfo.gameObject.GetComponent(GameObjectBehaviour).lane;
-
+Debug.Log("zomby collide");
 	//zombie attack human
 		collInfo.gameObject.rigidbody2D.velocity.x=0;
 		collInfo.gameObject.rigidbody2D.velocity.y=0;
