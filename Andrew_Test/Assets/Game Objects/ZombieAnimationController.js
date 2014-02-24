7daf612234d9fc75bd04794a908f56c1f1f00ead
Kimalye <kimalye@gmail.com>
2014-02-23 23:11:05 -0800

    What we did during lab: Big guy animation changes

--- Andrew_Test/Assets/Game Objects/ZombieAnimationController.js
+++ Andrew_Test/Assets/Game Objects/ZombieAnimationController.js
@@ -6,7 +6,11 @@ function Start () {
 
 function Update () {
 
+if(transform.rigidbody2D.velocity.x!=0){
+anim.SetInteger("State",0);
+}
+
 }
 function Attack(){
-anim.SetInteger("State",2);
+anim.SetInteger("State",1);
 }
