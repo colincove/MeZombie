eef750c9a5d51bccd3af7b90dc259c14dd6cba4b
Andrew Wong <andrew.wong.dgnt@gmail.com>
2014-03-03 19:09:57 -0800

    placeholder audio for zombies with script.  sound volume hasnt been adjusted yet.

---
+++ Andrew_Test/Assets/Game Objects/AudioController.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+
+private var hasDefaultSoundPlayed:boolean;
+var defaultSound: AudioSource;
+
+var attacking: AudioSource;
+
+var dying: AudioSource;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function Idle(){
+	if (!hasDefaultSoundPlayed){
+		StopAllSounds();
+		defaultSound.Play();
+		hasDefaultSoundPlayed=true;
+	}
+}
+
+function Attack(){
+	StopAllSounds();
+	attacking.Play();
+	hasDefaultSoundPlayed=false;
+}
+
+function OnKilled(){
+	StopAllSounds();
+	dying.Play();
+	
+	hasDefaultSoundPlayed=false;
+}
+
+private function StopAllSounds(){
+	defaultSound.Stop();
+	attacking.Stop();
+	dying.Stop();
+}
