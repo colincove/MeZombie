f5ccc4e86b1d42fbec32667a3578c0d0a7914e62
andrewwongdgnt <andrew.wong.dgnt@gmail.com>
2014-03-08 19:42:35 -0800

    barricade animation (noobish) and audio

--- Andrew_Test/Assets/Game Objects/AudioController.js
+++ Andrew_Test/Assets/Game Objects/AudioController.js
@@ -6,12 +6,19 @@ var idle: AudioSource;
 var attack: AudioSource;
 var die: AudioSource;
 
+var break1:AudioSource;
+var break2:AudioSource;
+var break3:AudioSource;
+
 private function StopAllSounds(){
 	if (walk!=null) walk.Stop();
 	if (idle!=null) idle.Stop();
 	if (attack!=null) attack.Stop();
-	
 	if (die!=null) die.Stop();
+	
+	if (break1!=null) break1.Stop();
+	if (break2!=null) break2.Stop();
+	if (break3!=null) break3.Stop();
 }
 
 function Start () {
@@ -48,11 +55,29 @@ function playDieSound(){
 		StopAllSounds();
 		die.Play();
 	}
-	
-	if (name=="Melee Hooman"){
-		Debug.Log("dead sound");
+
+}
+
+function playBarricadeBreak(i:int){
+	if (i<1) i=1;
+
+	if (break1!=null && i==1){
+		StopAllSounds();
+		break1.Play();
+	}
+	else if (break2!=null && i==2){
+		StopAllSounds();
+		break2.Play();
+	}
+	else if (break3!=null && i==3){
+		StopAllSounds();
+		break3.Play();
+	}
+	else {
+		Debug.Log("there is no break "+i+" sound");
 	}
 }
+
 
 function FadeOutSound(sound:AudioSource){
  
