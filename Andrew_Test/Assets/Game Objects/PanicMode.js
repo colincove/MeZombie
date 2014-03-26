5032a85ffac7abd4431a25f2565246cc7a2d43e2
andrewwongdgnt <andrew.wong.dgnt@gmail.com>
2014-03-26 13:37:37 -0700

    male child integrated

---
+++ Andrew_Test/Assets/Game Objects/PanicMode.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var velX :float;
+
+private var interval_start: float = 1;
+private var interval: float= interval_start;
+
+private var isKilled:boolean;
+
+function Start () {
+	rigidbody2D.velocity.x = -velX;
+}
+
+function Update () {
+	interval -= Time.deltaTime;
+	if (interval<=0 && !isKilled){
+		interval=interval_start;
+		rigidbody2D.velocity.x*=-1;
+		transform.localScale.x*=-1;
+	}
+}
+
+function OnKilled(){
+	isKilled=true;
+		rigidbody2D.velocity.x=0;
+}
