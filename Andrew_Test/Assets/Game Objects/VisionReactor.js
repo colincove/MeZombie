53beaae0deb099161374f0b6e27a9c8c5e36447b
andrewwongdgnt <andrew.wong.dgnt@gmail.com>
2014-03-28 05:26:20 -0700

    Fog of war

--- Andrew_Test/Assets/Game Objects/VisionReactor.js
+++ Andrew_Test/Assets/Game Objects/VisionReactor.js
@@ -1,13 +1,17 @@
 
 private var isVisible:boolean;
+
+var inverse:boolean;
 function Start(){
 	//gm = GameObject.Find("_gameMaster");
 	//vm = gm.GetComponent("VisionManager");
 	//vm.AddVisionObj(this);
 }
 function Update(){
+	var isVisible_new =  ( isVisible && !inverse ) || ( !isVisible && inverse );  //xor, used to invert expression
+	
 	if (transform.parent.gameObject.renderer!=null){
-		transform.parent.gameObject.renderer.enabled=isVisible;
+		transform.parent.gameObject.renderer.enabled=isVisible_new;
 	}else {
 		var allChildren = transform.parent.gameObject.GetComponentsInChildren(Transform);
  
@@ -15,7 +19,7 @@ function Update(){
 	    for (var child : Transform in allChildren) 
 	    {
 	    	if (child.renderer!=null)
-	    	child.renderer.enabled=isVisible;
+	    	child.renderer.enabled=isVisible_new;
 	    }
 	}
 		
